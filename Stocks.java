89e5e3862c4815f2385fd9899ae13afef8032f77
Abdul Rafay <abdurrafay20069@gmail.com>
2026-04-02 22:25:55 +0500

    Solved Best Time to Buy and Sell Stock using O(n) approach in Java

---
+++ Stocks.java
@@ -0,0 +1,27 @@
+public class Stocks {
+
+    // Function to calculate maximum profit
+    public static int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE; // minimum price so far
+        int profit = 0; // maximum profit
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i]; // update minimum price
+            } else {
+                int currentProfit = prices[i] - min; // profit if sold today
+                if (currentProfit > profit) {
+                    profit = currentProfit; // update max profit
+                }
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+    }
+}
